ef99b72c0f1f2d68baabb57a5c7a3065e13ae0a0
chda6313 <chda6313@colorado.edu>
2017-03-21 15:39:58 -0600

    put an insert line in to begin to populate our database

--- Maker_Game.sql
+++ Maker_Game.sql
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 create database if not exists Maker_Game;
 
 use Maker_Game; 
 
-=======
->>>>>>> 48b76db6cbaba7d6bc66d3b3139966b1cb28185c
- create table if not exists Levels(id integer primary key auto_increment,rating integer, publishment date, creator char(20), high_score integer);
+create table if not exists Levels(id integer primary key auto_increment,rating integer, publishment date, creator char(20), high_score integer, name char(20));
 
 create table if not exists Platforms(id integer primary key, rect geometry); 
 
 create table if not exists Coins(id integer primary key, coin geometry);
 
- 
+
+/*example of populating a table*/
+insert into Levels values (1, 10, str_to_date('3-21-2017'), 'chda6313', 20, 'exlevel1');
