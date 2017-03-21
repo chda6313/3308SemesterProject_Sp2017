79dfde8b7c65ef4c8e1071260b8a29d78ca7d30b
hamiltonmnelson <hamiltonmnelson@gmail.com>
2017-03-21 15:22:53 -0600

    Database population file - Incomplete until level generator is completed

---
+++ SQLInsertHandler.php
@@ -0,0 +1,21 @@
+<?php
+$connection = @mysqli_connect ('127.0.0.1','root', 'Nellie95','Maker_Game');
+$Id = mysql_insert_id()+1;
+$Name = $_REQUEST['User_Name'];
+$Publish = date("d.m.Y");
+$Rating = $_REQUEST['Rating'];
+$High_Score = NULL;
+
+$Rectangle = NULL;
+$Coin = NULL;
+
+$insertMain = "INSERT INTO store (id, name, qty, price) VALUES ('$Id','$Name','$Quantity','$Price')";
+$insertRect = "INSERT INTO Platforms(id, rect) VALUES ('$Id','$Rectangle')";
+$insertCoin = "INSERT INTO Coins(id, coin) VALUES ('$Id','$Coin')";
+
+mysqli_query($connection, $insertMain);
+mysqli_query($connection, $insertRect);
+mysqli_query($connection, $insertCoin);
+
+}
+?>
